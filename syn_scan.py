3ea2b6a6ba6115608f3428740cf315e7980ecbaa
sean <seanlcarstensen@gmail.com>
2018-03-16 20:20:39 -0700

    incorporated ThreadPool class to make scan faster

--- syn_scan.py
+++ syn_scan.py
@@ -1,8 +1,8 @@
 # usage syn_scan lowPort highPort
+from multiprocessing.pool import ThreadPool
 
 import sys
 from scapy.all import *
-import threading
 
 def scan_port(prt):
     pkt = IP(dst="scanme.nmap.org")/TCP(dport = prt, flags="S")
@@ -11,12 +11,27 @@ def scan_port(prt):
     if respFlags == 18:
         openPorts.append(prt)
 
+def build_ports():
+    for n in range(lowPrt, highPrt):
+        ports.append(n)
+
+def scan_ports():
+    pool = ThreadPool()
+    pool.map(scan_port, ports)
+
+def print_open_ports():
+    for prt in openPorts:
+        print("%s is open" % prt)
+
+
 lowPrt = int(sys.argv[1])
 highPrt = int(sys.argv[2])
 openPorts = []
+ports = []
+threads = []
 
-for i in range(lowPrt, highPrt + 1):
-    scan_port(i)
+build_ports()
 
-for prt in openPorts:
-    print("%s is open" % prt)
+scan_ports()
+
+print_open_ports()
